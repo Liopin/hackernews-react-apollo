8520d29b8a9687452c19ed8c0ed73a245c9f4655
Liopin <contact.pinheiro.lionel@gmail.com>
2020-05-05 18:33:31 +0200

    Routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
 import '../styles/App.css';
 import LinkList from './LinkList'
-import CreateLink from './CreateLink';
+import CreateLink from './CreateLink'
+import Header from './Header'
+
 
 
 
 class App extends Component{
   render(){
     return (
-      <>
-      <LinkList></LinkList>
-      <CreateLink></CreateLink>
-      </>
+      <div className="center w85">
+        <Header></Header>
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList}/>
+            <Route exact path="/create" component={CreateLink}/>
+          </Switch>
+        </div>
+      </div>
     )
   }
 }
