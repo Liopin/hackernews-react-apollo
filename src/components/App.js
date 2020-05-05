e39de6345cebe508576294cd59b59f5656b8ad5a
Liopin <contact.pinheiro.lionel@gmail.com>
2020-05-05 18:01:43 +0200

    Creating createLink component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import LinkList from './LinkList'
+import CreateLink from './CreateLink';
+
 
 
 class App extends Component{
   render(){
-    return <LinkList></LinkList>
+    return (
+      <>
+      <LinkList></LinkList>
+      <CreateLink></CreateLink>
+      </>
+    )
   }
 }
 
